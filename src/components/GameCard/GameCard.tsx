7e91748c0d99973ba84964b8d5be3c6945acc94a
yogeshdnumb <dyogesh.official@gmail.com>
2024-05-21 01:45:51 +0530

    added Loader to card

--- src/components/GameCard/GameCard.tsx
+++ src/components/GameCard/GameCard.tsx
@@ -1,11 +1,23 @@
 import styles from "./GameCard.module.scss";
 import { Link } from "react-router-dom";
+import Loader from "components/Loader/Loader";
+import { useState } from "react";
 
 export default function GameCard({ img, title, gameId }) {
+  const [isImgLoading, setIsImgLoading] = useState(true);
+
   return (
     <div className={styles.GameCard}>
       <Link to={`/game/${gameId}`}>
-        <img src={img} alt="" />
+        {isImgLoading && <Loader size={100}></Loader>}
+        <img
+          src={img}
+          className={isImgLoading ? styles.hidden : styles.cardImg}
+          alt=""
+          onLoad={() => {
+            setIsImgLoading(false);
+          }}
+        />
         <p className={styles.title}>{title}</p>
       </Link>
     </div>
